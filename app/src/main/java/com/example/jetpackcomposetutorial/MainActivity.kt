d8d4dbf729964be152c34e9e80363312b0466af6
rogeliocarrilloru <rrcr1975@gmail.com>
2023-10-31 01:21:46 +0100

    1

--- app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -31,10 +32,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+   Column {
+       Text(
+           text = "Hello $name!",
+           modifier = modifier
+       )
+       Text(text = "Rogelio")
+   }
 }
 
 @Preview(showBackground = true)
